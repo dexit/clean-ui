a0e5c3f7ad55a28f94301b91f4d1aa54f08a0818
Vincent Klaiber <vinkla@users.noreply.github.com>
2021-03-04 09:16:43 +0100

    Update clean-ui.php

--- clean-ui.php
+++ clean-ui.php
@@ -28,25 +28,18 @@ add_action('admin_init', 'clean_ui_menu_items');
 
 function clean_ui_toolbar_items($menu): void
 {
-    $items = [
-        'comments', // Comments
-        'customize', // Customize
-        'edit', // Edit
-        'new-content', // New Content
-        'search', // Search
-        'updates', // Updates
-        // 'site-name', // Site Name
-        'view-site', // Visit Site
-        'dashboard', // Dashboard
-        'themes', // Themes
-        'menus', // Menus
-        'widgets', // Widgets
-        'wp-logo', // WordPress Logo
-    ];
-
-    foreach ($items as $item) {
-        $menu->remove_node($item);
-    }
+    $menu->remove_node('comments'); // Comments
+    $menu->remove_node('customize'); // Customize
+    $menu->remove_node('edit'); // Edit
+    $menu->remove_node('new-content'); // New Content
+    $menu->remove_node('search'); // Search
+    $menu->remove_node('updates'); // Updates
+    // $menu->remove_node('site-name'); // Site Name
+    $menu->remove_node('view-site'); // Visit Site
+    $menu->remove_node('dashboard'); // Dashboard
+    $menu->remove_node('themes'); // Themes
+    $menu->remove_node('menus'); // Menus
+    $menu->remove_node('wp-logo'); // WordPress Logo
 }
 
 add_action('admin_bar_menu', 'clean_ui_toolbar_items', 999);
